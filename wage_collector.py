5558d13883664dc958d49d3888b3eb7aa0665aa3
Osvin Alaphat <140211517+osvinalaphat@users.noreply.github.com>
2025-06-11 11:02:25 -0500

    add wage collector files

---
+++ wage_collector.py
@@ -0,0 +1,47 @@
+# hours worked daily
+# hourly pay rate
+
+#PROCESSING
+#yearly pay = 350 * daily_rate * daily hours 
+#tax amount = yearly pay * 0.12
+#yearly pay with tax deduction = yearly pay -tax amount
+
+# before tax deduction
+# tax deduction amount 
+# after tax deduction 
+
+#first ask for hours worked and pay rate
+
+def main():
+    #get info
+    #come back for while loops!!!!!!!!!!!!
+    while True:
+        try:
+            daily_hours_worked = float(input("Enter the number of hours worked daily: "))
+            if daily_hours_worked < 0 or daily_hours_worked >24:
+                print("ERROR: Hours must be between 0 and 24")
+                continue
+            daily_pay_rate = float(input("Enter the hourly wage: "))
+            if daily_pay_rate <0:
+                 print("ERROR: Pay rate must be more than 0")
+                 continue
+        except:
+            print("Please enter numbers")
+            continue
+        break
+
+    yearly_pay_no_taxes = daily_hours_worked * daily_pay_rate * 350
+    tax_amount = yearly_pay_no_taxes*0.12
+    yearly_pay_with_taxes = yearly_pay_no_taxes - tax_amount
+
+    print("\n")
+    print("Pay Advice")
+    print("------------")
+    print(f"Hours Worked: {daily_hours_worked:.2f}")
+    print(f"Hourly Wage: ${daily_pay_rate:.2f}")
+    print(f"Wages Before Taxes: ${yearly_pay_no_taxes:.2f}")
+    print(f"Tax Amount: ${tax_amount:.2f}")
+    print(f"Annual Wage After Taxes: ${yearly_pay_with_taxes:.2f}")
+
+
+main()
