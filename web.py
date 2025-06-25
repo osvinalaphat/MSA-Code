2c0f23efb73efe65c1549fe61e7c9e3344fbde3c
Osvin Alaphat <140211517+osvinalaphat@users.noreply.github.com>
2025-06-25 11:26:06 -0500

    Update web.py

--- web.py
+++ web.py
@@ -1,6 +1,7 @@
 import flask
 #flask is a server
 from flask import request,jsonify
+import student_generator_v2 as sg
 
 # create a flask app object
 
@@ -19,10 +20,33 @@ app.config["DEBUG"] = True
 def index():
     return "<h1> My name is Osvin Alaphat </h1>"
 
+#create a route to return all student data
+@app.route('/api/students/all',methods=['GET'])
+def api_all():
+    #load student dictionaries
+    student_dictionaries = sg.get_student_dictionary()
+    return jsonify(student_dictionaries)
+
+#create a rpite tp return students by major
+@app.route('/api/majors/<string:major>', methods=['GET'])
+def api_students_by_major(major):
+    print(major)
+    #get students with that major
+    #get all students
+    student_dictionaries = sg.get_student_dictionary()
+    major_students = []
+
+    for student in student_dictionaries:
+        if major.lower() == student['major'].lower():
+            major_students.append(student)
+    return jsonify(major_students)
+
+
 # create 2 routes
 
-# 1 route - return all student data
+# 1 route - return all student data  
 # 1 route - return students by major
 
 # run the application
 app.run()
+#
